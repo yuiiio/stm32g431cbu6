c0182257c570858b11c40d32548c6f198a01ab05
yuiiio <atbjyk@protonmail.com>
2023-10-23 23:14:37 +0900

    switch fft impl

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,82 @@ use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
 use st7789::{Orientation, ST7789};
 
-use microfft;
+// reference from https://github.com/vha3/Hunter-Adams-RP2040-Demos/blob/master/Audio/g_Audio_FFT/fft.c
+const NUM_SAMPLES: usize = 256;
+const LOG2_NUM_SAMPLES: u16 = 8;// 256 = 2^8
+// Length of short (16 bits) minus log2 number of samples (6)
+const SHIFT_AMOUNT: u16 = 16 - LOG2_NUM_SAMPLES;
+
+fn multfix15(a: i16, b: i16) -> i16 {
+    ((a as i32 * b as i32) >> 15) as i16
+}
+
+fn fftfix(fr: &mut [i16; NUM_SAMPLES], fi: &mut [i16; NUM_SAMPLES], sinewave: &[i16; NUM_SAMPLES]) -> () {
+    //bit order reverse
+    for m in 1..(NUM_SAMPLES - 1) {
+        // swap odd and even bits
+        let mut mr = ((m >> 1) & 0x5555) | ((m & 0x5555) << 1);
+        // swap consecutive pairs
+        mr = ((mr >> 2) & 0x3333) | ((mr & 0x3333) << 2);
+        // swap nibbles ... 
+        mr = ((mr >> 4) & 0x0F0F) | ((mr & 0x0F0F) << 4);
+        // swap bytes
+        mr = ((mr >> 8) & 0x00FF) | ((mr & 0x00FF) << 8);
+        // shift down mr
+        mr >>= SHIFT_AMOUNT ;
+        // don't swap that which has already been swapped
+        if mr<=m { continue; }
+        // swap the bit-reveresed indices
+        let tr = fr[m] ;
+        fr[m] = fr[mr] ;
+        fr[mr] = tr ;
+        let ti = fi[m] ;
+        fi[m] = fi[mr] ;
+        fi[mr] = ti ;
+    }
+    //println!("{:?}", fr);
+    // Adapted from code by:
+    // Tom Roberts 11/8/89 and Malcolm Slaney 12/15/94 malcolm@interval.com
+    // Length of the FFT's being combined (starts at 1)
+    let mut l: usize = 1 ;
+    // Log2 of number of samples, minus 1
+    let mut k: u16 = LOG2_NUM_SAMPLES - 1 ;
+    // While the length of the FFT's being combined is less than the number 
+    // of gathered samples . . .
+    while l < NUM_SAMPLES {
+        // Determine the length of the FFT which will result from combining two FFT's
+        let istep: usize = l << 1 ;
+        // For each element in the FFT's that are being combined . . .
+        for m in 0..l {
+            let j = m << k;
+            let mut wr: i16 =  sinewave[j + (NUM_SAMPLES / 4)] ; // cos(2pi m/N)
+            let mut wi: i16 = -sinewave[j] ;                 // sin(2pi m/N)
+            wr >>= 1 ;                          // divide by two
+            wi >>= 1 ;                          // divide by two
+            // i gets the index of one of the FFT elements being combined
+            let mut i: usize = m;
+            while i < NUM_SAMPLES {
+                // j gets the index of the FFT element being combined with i
+                let j = i + l ;
+                // compute the trig terms (bottom half of the above matrix)
+                let tr = multfix15(wr, fr[j]) - multfix15(wi, fi[j]) ;
+                let ti = multfix15(wr, fi[j]) + multfix15(wi, fr[j]) ;
+                // divide ith index elements by two (top half of above matrix)
+                let qr = fr[i] >> 1 ;
+                let qi = fi[i] >> 1 ;
+                // compute the new values at each index
+                fr[j] = qr - tr ;
+                fi[j] = qi - ti ;
+                fr[i] = qr + tr ;
+                fi[i] = qi + ti ;
+
+                i += istep;
+            }
+        }
+        k = k - 1;
+        l = istep;
+    }
+}
 
 #[entry]
 fn main() -> ! {
@@ -125,6 +200,12 @@ fn main() -> ! {
 
     //adc.configure_channel(&adc_ch0, Sequence::One, SampleTime::Cycles_480); // need 480 cycles from datasheet
 
+        // should calc once
+    let mut sinewave: [i16; NUM_SAMPLES] = [0; NUM_SAMPLES];
+    for i in 0..NUM_SAMPLES {
+        sinewave[i] = ((6.283 * (i as f32 / NUM_SAMPLES as f32)).sin() as f32 * 32768.0 as f32) as i16; // float2fix15 //2^15
+    }
+
     let buffer1: &mut [u8; 240] = &mut [0; 240];
     let buffer2: &mut [u8; 240] = &mut [0; 240];
     let mut flip: bool = true;
@@ -134,13 +215,13 @@ fn main() -> ! {
             adc_results[i] = adc.convert(&adc_ch0, SampleTime::Cycles_480);
         }
 
-        let mut samples: [f32; 256] = [0.0; 256];
-        for i in 0..256 {
-            samples[i] = adc_results[i] as f32;
+        let mut fr: [i16; NUM_SAMPLES] = [0; NUM_SAMPLES];
+        let mut fi: [i16; NUM_SAMPLES] = [0; NUM_SAMPLES];
+        for i in 0..NUM_SAMPLES {
+            fr[i] = adc_results[i] as i16;
         }
 
-        let spectrum = microfft::real::rfft_256(&mut samples);
-        spectrum[0].im = 0.0; // need ?
+        fftfix(&mut fr, &mut fi, &sinewave);
 
         let mut buffer: &mut [u8; 240] = &mut [0; 240];
         let mut prev: &mut [u8; 240] = &mut [0; 240];
@@ -157,7 +238,7 @@ fn main() -> ! {
         let mut max: u32 = 0;
         let mut amplitudes: [u32; 240] = [0; 240];
         for i in 0..128 { // 128 ~ 240 should 00
-            amplitudes[i] = (spectrum[i].re.abs() + spectrum[i].im.abs()) as u32;
+            amplitudes[i] = (fr[i].abs() + fi[i].abs()) as u32;
 
             if amplitudes[i] > max {
                 max = amplitudes[i];
